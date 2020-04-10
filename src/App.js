d49863798f61cafe75134f4e7ac260ea5f97ad27
Tushar <31848799+tushardahiya@users.noreply.github.com>
2020-04-10 15:30:42 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+  state ={
+    persons:[
+      {id:'1',name:'max',age:28},
+      {id:'2',name:'tushar',age:21},
+      {id:'3',name:'mayank',age:24}
+    ],
+    otherState:'some other value',
+    showPersons:false
+  };
+
+  nameChangedHandler=(event,id) => {
+
+    const personIndex= this.state.persons.findIndex(p=>{
+      return p.id===id;
+    })
+    const person = {...this.state.persons[personIndex]};
+    person.name=event.target.value;
+    const persons=[...this.state.persons];
+    persons[personIndex]=person;
+    this.setState({
+      persons:persons
+    })
+  }
+
+  togglePersonsHandler = () => {
+      const doesShow = this.state.showPersons;
+      this.setState({
+        showPersons:!doesShow
+      })
+  }
+
+  deleteNameHandler = (personIndex) => {
+      // const persons = this.state.persons.slice(); //first way of getting a new array and not overridding the OG array
+      const persons = [...this.state.persons]; // the more modern way of getting a new array and not overridding the OG array
+      persons.splice(personIndex,1);
+      this.setState({persons:persons})
+  }
+
+
+  render() {
+    const style = {
+      backgroundColor:'white',
+      font:'inherit',
+      border:'1px solid blue',
+      padding:'8px',
+      cursor:'pointer'
+    };
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (    
+          <div>
+              {this.state.persons.map( (person,index)=> {
+                return <Person 
+                click={this.deleteNameHandler.bind(this,index)}
+                name={person.name} 
+                age={person.age}
+                key={person.id}
+                changed={(event)=>this.nameChangedHandler(event,person.id)} />
+              })
+              }
+          </div>
+        );
+}
+
+    return (
+      <div className='App'>
+        <h1>hi i'm a react App</h1>
+        <p>wow this is really working !</p>
+        <button
+        style={style} 
+        onClick={this.togglePersonsHandler} >Toggle Persons</button>
+        {persons}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Person.css';
+
+const person = (props) => {
+    return (
+        <div className="Person">
+            <p onClick={props.click} >i'm {props.name} and i'm {props.age} years old !</p>
+            <p>{props.children}</p>
+            <input onChange={props.changed} value={props.name}/>
+        </div>
+    )
+};
+
+export default person;
